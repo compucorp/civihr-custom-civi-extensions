37013813987c303f8766da22a33d01cdfffe86f4
kacperHex <kacper@hexdigital.com>
2014-12-03 17:09:31 +0100

    New Unit Tests: Add Revision test + Edit Revision test.

---
+++ hrjobcontract/tests/phpunit/CRM/Hrjobcontract/AddRevisionTest.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once 'HRJobContractTestBase.php';
+
+/**
+ * FIXME
+ */
+class CRM_Hrjobcontract_AddRevisionTest extends HRJobContractTestBase {
+  function setUp() {
+    parent::setUp();
+  }
+
+  function tearDown() {
+    parent::tearDown();
+  }
+
+  /**
+   * Test Job Contract Revision after adding changes to the entities.
+   */
+  function testAddRevision() {
+    
+    $expected = array(
+        "id" => "10",
+        "jobcontract_id" => "1",
+        "data_revision_id" => "10",
+        "health_revision_id" => "2",
+        "role_revision_id" => "3",
+        "hour_revision_id" => "4",
+        "pay_revision_id" => "9",
+        "leave_revision_id" => "6",
+        "pension_revision_id" => "7",
+    );
+    
+    
+    $this->createJobContract();
+    $this->createJobContractEntities(1);
+    
+    // Adding jobcontract_data entity:
+    civicrm_api3('HRJobData', 'create', array(
+      'title' => "new title",
+      'jobcontract_id' => 1,
+    ));
+    
+    // Adding jobcontract_pay entity:
+    civicrm_api3('HRJobPay', 'create', array(
+      'pay_is_auto_est' => 5,
+      'pay_amount' => 3,
+      'jobcontract_id' => 1,
+    ));
+    
+    // Adding jobcontract_data entity again:
+    civicrm_api3('HRJobData', 'create', array(
+      'title' => "newest title",
+      'jobcontract_id' => 1,
+    ));
+      
+    $current_revision = civicrm_api3('HRJobContractRevision', 'getcurrentrevision', array(
+        'sequential' => 1,
+        'jobcontract_id' => 1,
+    ));
+    
+    $this->assertAPIArrayComparison($current_revision['values'], $expected);
+  }
+}

e6757a6690ceaac3b738a79341207cae6302277b
Mateusz Cieplinski <mateusz@coldrun.pl>
2014-12-09 13:21:43 +0100

    services changes

--- hrjobcontract/js/services/contractHour.js
+++ hrjobcontract/js/services/contractHour.js
@@ -1,20 +1,43 @@
 console.log('ContractHourService');
-define(['services/services'], function (services) {
-    services.factory('ContractHourService', function () {
+define(['services/services','filters/getObjByContractId'], function (services) {
+    services.factory('ContractHourService', function ($filter) {
         var items = {};
 
-        items.query = function () {
-            return {
-                "values":[{
-                    "id":"1",
-                    "hours_type":"8",
-                    "hours_amount":"8",
-                    "hours_unit":"Day",
-                    "fte_num":"1",
-                    "fte_denom":"1",
-                    "jobcontract_revision_id": "11"
+        items.query = function (id) {
+            var response = {
+                "values": [{
+                    "values":[{
+                        "contract_id":"1",
+                        "id":"1",
+                        "hours_type":"8",
+                        "hours_amount":"8",
+                        "hours_unit":"Day",
+                        "fte_num":"1",
+                        "fte_denom":"1",
+                        "jobcontract_revision_id": "14"
+                    },{
+                        "contract_id":"2",
+                        "id":"2",
+                        "hours_type":"8",
+                        "hours_amount":"8",
+                        "hours_unit":"Day",
+                        "fte_num":"1",
+                        "fte_denom":"1",
+                        "jobcontract_revision_id": "15"
+                    },{
+                        "contract_id":"3",
+                        "id":"3",
+                        "hours_type":"8",
+                        "hours_amount":"8",
+                        "hours_unit":"Day",
+                        "fte_num":"1",
+                        "fte_denom":"1",
+                        "jobcontract_revision_id": "16"
+                    }]
                 }]
-            };
+            }
+
+            return typeof id !== 'undefined' ? $filter('getObjByContractId')(response.values, id) || response.values : response.values;
         };
         return items;
     });
--- hrjobcontract/js/services/contractPension.js
+++ hrjobcontract/js/services/contractPension.js
@@ -1,20 +1,41 @@
 console.log('ContractPensionService');
-define(['services/services'], function (services) {
-    services.factory('ContractPensionService', function () {
+define(['services/services','filters/getObjByContractId'], function (services) {
+    services.factory('ContractPensionService', function ($filter) {
         var items = {};
 
-        items.query = function () {
-            return {
+        items.query = function (id) {
+            var response = {
                 "values":[{
+                    "contract_id":"1",
                     "id":"1",
                     "is_enrolled":"0",
                     "ee_contrib_pct":"2",
                     "er_contrib_pct":"10",
                     "pension_type":"Employer Pension",
                     "ee_contrib_abs":"0.00",
-                    "jobcontract_revision_id": "11"
+                    "jobcontract_revision_id": "23"
+                },{
+                    "contract_id":"2",
+                    "id":"2",
+                    "is_enrolled":"0",
+                    "ee_contrib_pct":"2",
+                    "er_contrib_pct":"10",
+                    "pension_type":"Employer Pension",
+                    "ee_contrib_abs":"0.00",
+                    "jobcontract_revision_id": "24"
+                },{
+                    "contract_id":"3",
+                    "id":"3",
+                    "is_enrolled":"0",
+                    "ee_contrib_pct":"2",
+                    "er_contrib_pct":"10",
+                    "pension_type":"Employer Pension",
+                    "ee_contrib_abs":"0.00",
+                    "jobcontract_revision_id": "25"
                 }]
-            };
+            }
+
+            return typeof id !== 'undefined' ? $filter('getObjByContractId')(response.values, id) || response.values : response.values;
         };
         return items;
     });

b2e0f7aaf6acf5dd4f5651d7e62a6f43065e9b22
Mateusz Cieplinski <mateusz@coldrun.pl>
2014-12-10 17:16:44 +0100

    cancel modal

--- hrjobcontract/js/controllers/modalForm.js
+++ hrjobcontract/js/controllers/modalForm.js
@@ -2,6 +2,8 @@ console.log('Controller: ModalFormCtrl');
 define(['controllers/controllers'], function(controllers){
     controllers.controller('ModalFormCtrl',['$scope','$modalInstance',
         function($scope, $modalInstance){
-
+            $scope.cancel = function () {
+                $modalInstance.dismiss('cancel');
+            };
         }]);
 });

57852173f889bd55fc1565781916b0743d1c0ad4
Mateusz Cieplinski <mateusz@coldrun.pl>
2014-12-17 03:05:01 +0100

    View contract: Leave

--- hrjobcontract/js/controllers/modal/modalContractView.js
+++ hrjobcontract/js/controllers/modal/modalContractView.js
@@ -1,12 +1,13 @@
 console.log('Controller: ModalContractViewCtrl');
 define(['controllers/controllers'], function(controllers){
-    controllers.controller('ModalContractViewCtrl',['$scope','$modalInstance','contract',
-        function($scope, $modalInstance, contract){
+    controllers.controller('ModalContractViewCtrl',['$scope','$modalInstance','contract','utils',
+        function($scope, $modalInstance, contract, utils){
 
-            $scope.contract = {};
-            $scope.title = 'Contract';
             $scope.allowSave = false;
+            $scope.contract = {};
             $scope.isDisabled = true;
+            $scope.title = 'Contract';
+            $scope.utils = utils;
 
             angular.copy(contract,$scope.contract);
 

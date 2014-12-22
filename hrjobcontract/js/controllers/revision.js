8d0b4e79b0921efedf110586d9e3f8b130406f45
Mateusz Cieplinski <mateusz@coldrun.pl>
2014-12-22 16:34:48 +0100

    Full History - list revisions

--- hrjobcontract/js/controllers/revision.js
+++ hrjobcontract/js/controllers/revision.js
@@ -1,15 +1,15 @@
-console.log('Controller: HistoryCtrl');
+console.log('Controller: RevisionCtrl');
 define(['controllers/controllers',
-        'services/contractDetails',
-        'services/contractLeave',
-        'services/contractPension',
-        'services/contractInsurance'], function(controllers){
-    controllers.controller('HistoryCtrl',['$scope', '$route', '$modal', '$rootElement', '$q', 'settings',
+    'services/contractDetails',
+    'services/contractLeave',
+    'services/contractPension',
+    'services/contractInsurance'], function(controllers){
+    controllers.controller('RevisionCtrl',['$scope', '$route', '$modal', '$rootElement', '$q', 'settings',
         'ContractDetailsService', 'ContractLeaveService', 'ContractInsuranceService','ContractPensionService',
         function($scope, $route, $modal, $rootElement, $q, settings, ContractDetailsService,
                  ContractLeaveService, ContractInsuranceService, ContractPensionService){
 
-
+            console.log($scope.revision);
 
         }]);
 });

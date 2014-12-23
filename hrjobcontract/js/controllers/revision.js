2a40ed1dddb8154cde271a86d77b461ba0aec41c
Mateusz Cieplinski <mateusz@coldrun.pl>
2014-12-23 02:37:49 +0100

    display revisions

--- hrjobcontract/js/controllers/revision.js
+++ hrjobcontract/js/controllers/revision.js
@@ -9,16 +9,11 @@ define(['controllers/controllers',
         function($scope, $route, $modal, $rootElement, $q, settings, ContractDetailsService,
                  ContractLeaveService, ContractInsuranceService, ContractPensionService){
 
-            var promiseContractDetails = ContractDetailsService.getOne({ jobcontract_revision_id: $scope.revision.details_revision_id }),
-                promiseContractLeave = ContractLeaveService.get({ jobcontract_revision_id: $scope.revision.leave_revision_id }),
-                promiseContractInsurance = ContractInsuranceService.getOne({ jobcontract_revision_id: $scope.revision.health_revision_id }),
-                promiseContractPension = ContractPensionService.getOne({ jobcontract_revision_id: $scope.revision.pension_revision_id });
-
             $q.all({
-                details: promiseContractDetails,
-                leave: promiseContractLeave,
-                insurance: promiseContractInsurance,
-                pension: promiseContractPension
+                details: ContractDetailsService.getOne({ jobcontract_revision_id: $scope.revision.details_revision_id }),
+                leave: ContractLeaveService.get({ jobcontract_revision_id: $scope.revision.leave_revision_id }),
+                insurance: ContractInsuranceService.getOne({ jobcontract_revision_id: $scope.revision.health_revision_id }),
+                pension: ContractPensionService.getOne({ jobcontract_revision_id: $scope.revision.pension_revision_id })
             }).then(function(results){
                 $scope.details = results.details;
                 $scope.leave = results.leave;

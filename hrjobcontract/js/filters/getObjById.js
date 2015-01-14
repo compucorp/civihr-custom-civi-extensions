0ff56e2016ea72f54ebccafe33bce52e56aa681d
Mateusz Cieplinski <mateusz@coldrun.pl>
2015-01-14 15:31:21 +0100

    Entity: Pay

--- hrjobcontract/js/filters/getObjById.js
+++ hrjobcontract/js/filters/getObjById.js
@@ -1,11 +1,11 @@
 console.log('Filter: getObjById');
 define(['filters/filters'], function(filters){
     filters.filter('getObjById',function(){
-        return function(input, id) {
+        return function(input, id, key) {
             var i=0, len=input.length;
             for (; i<len; i++) {
                 if (+input[i].id == +id) {
-                    return input[i];
+                    return !key ? input[i] : input[i][key];
                 }
             }
             return null;

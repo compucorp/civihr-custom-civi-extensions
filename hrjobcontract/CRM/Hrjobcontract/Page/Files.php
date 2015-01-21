1f7a503949d0d2d4434e6eb71ea0ceb85c109595
kacperHex <kacper@hexdigital.com>
2015-01-21 17:26:17 +0100

    Decoding html entities for list() method JSON response.

--- hrjobcontract/CRM/Hrjobcontract/Page/Files.php
+++ hrjobcontract/CRM/Hrjobcontract/Page/Files.php
@@ -66,7 +66,7 @@ class CRM_Hrjobcontract_Page_Files extends CRM_Core_Page {
       }
     }
     
-    echo stripcslashes(json_encode(array('values' => $result)));
+    echo html_entity_decode(stripcslashes(json_encode(array('values' => $result))));
     CRM_Utils_System::civiExit( );
   }
   public static function fileDisplay() {// Display evidence file
